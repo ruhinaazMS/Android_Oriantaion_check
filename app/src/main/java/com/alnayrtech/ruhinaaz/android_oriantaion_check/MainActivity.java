31b64bf020dd80022bab01524ca2f06a577f3cb7
ruhinaazMS <37713783+ruhinaazMS@users.noreply.github.com>
2018-03-24 01:46:38 +0530

    Initial commit

---
+++ app/src/main/java/com/alnayrtech/ruhinaaz/android_oriantaion_check/MainActivity.java
@@ -0,0 +1,25 @@
+package com.alnayrtech.ruhinaaz.android_oriantaion_check;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int orientation = this.getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            //code for portrait mode
+            Toast.makeText(MainActivity.this,"Oriatation portrait",Toast.LENGTH_LONG).show();
+        } else {
+
+            Toast.makeText(MainActivity.this,"Oriatation landscapt",Toast.LENGTH_LONG).show();
+            //code for landscape mode
+        }
+    }
+}
